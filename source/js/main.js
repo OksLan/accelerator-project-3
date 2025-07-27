1287d840e3427ee072c70332df7ab0c8eac58015
Oksana Lanovenko <okslan@gmail.com>
2025-07-27 19:07:47 +0400

    27/07 (1)

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,11 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+
+/* FAQ */
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.faq__details').forEach((el, i) => {
+    el.open = (i === 2);
+  });
+});

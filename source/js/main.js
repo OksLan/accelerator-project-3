99bdee4f613634f408a520c193a3f88b95599a4f
Oksana Lanovenko <okslan@gmail.com>
2025-08-03 19:23:14 +0400

    03/08 (1)

--- source/js/main.js
+++ source/js/main.js
@@ -3,7 +3,6 @@
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
 
-
 /* FAQ */
 // document.addEventListener('DOMContentLoaded', () => {
 //   document.querySelectorAll('.faq__details').forEach((el, i) => {
@@ -12,5 +11,50 @@
 // });
 
 
-/* ABOUT - Смена икноки arrow на кнопке */
+/* ABOUT - Смена икноки на кнопке */
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('.about__button');
+  const svgUse = button?.querySelector('use');
+  const XLINK_NS = 'http://www.w3.org/1999/xlink';
 
+  if (!button || !svgUse) {
+    console.log('Кнопка или <use> не найдены');
+    return;
+  }
+
+  const setIcon = (href) => {
+    svgUse.setAttributeNS(XLINK_NS, 'xlink:href', href);
+    svgUse.setAttribute('href', href);
+    console.log('установлена иконка', href);
+  };
+
+  button.addEventListener('mouseenter', () => {
+    console.log('hover');
+    setIcon('/__spritemap#sprite-arrow-hv-light');
+  });
+
+  button.addEventListener('mouseleave', () => {
+    console.log('leave');
+    setIcon('/__spritemap#sprite-arrow-df-light');
+  });
+
+  button.addEventListener('mousedown', () => {
+    console.log('mousedown');
+    setIcon('/__spritemap#sprite-arrow-act-light');
+  });
+
+  button.addEventListener('mouseup', () => {
+    console.log('mouseup');
+    setIcon('/__spritemap#sprite-arrow-hv-light');
+  });
+
+  button.addEventListener('focus', () => {
+    console.log('focus');
+    setIcon('/__spritemap#sprite-arrow-fc-light');
+  });
+
+  button.addEventListener('blur', () => {
+    console.log('blur');
+    setIcon('/__spritemap#sprite-arrow-df-light');
+  });
+});

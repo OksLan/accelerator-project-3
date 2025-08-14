42db7040494028b0bb5ee4278d4c6ee2b776717b
Oksana Lanovenko <158132001+OksLan@users.noreply.github.com>
2025-08-14 15:27:15 +0400

    14/08 (2)

--- source/js/main.js
+++ source/js/main.js
@@ -9,3 +9,22 @@
 //     el.open = (i === 2);
 //   });
 // });
+
+/* развернуть/свернуть main__nav */
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.querySelector(".hero__header-toggle");
+  const navList = document.querySelector(".main__nav");
+  const icon = toggleButton.querySelector("use");
+
+  toggleButton.addEventListener("click", () => {
+    toggleButton.classList.toggle("hero__header-toggle--open");
+    toggleButton.classList.toggle("hero__header-toggle--close");
+    navList.classList.toggle("main__nav--opened");
+
+    const isOpen = navList.classList.contains("main__nav--opened");
+    icon.setAttribute(
+      "href",
+      isOpen ? "/__spritemap#sprite-cross" : "/__spritemap#sprite-burger"
+    );
+  });
+});
